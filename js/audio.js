cd13f78208910a4f1fe5f6379fe7e4ddcc26f129
Your Name <tatsuuehira@Tatsus-MBP.hitronhub.home>
2020-06-22 02:09:10 -0400

    Add a js folder, and js files

---
+++ js/audio.js
@@ -0,0 +1,11 @@
+// Audio for Hover and Click
+// Audio ID 1
+let audio_1 = document.getElementById('audioID_1');
+//Example of an HTML Audio/Video Method
+function playAudio_1() {
+  audio_1.currentTime = 0;
+  audio_1.volume = 0.1;
+  // audio.load();
+  audio_1.play();
+}
+
